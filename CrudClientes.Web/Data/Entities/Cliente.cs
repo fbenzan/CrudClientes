4443676d92326485a2c5cf8fc8ee8d4ccec78546
Felix Benzan <felixbenz07@gmail.com>
2025-06-18 18:29:00 -0400

    agrege un espacio en la clase Cliente

--- CrudClientes.Web/Data/Entities/Cliente.cs
+++ CrudClientes.Web/Data/Entities/Cliente.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace CrudClientes.Web.Data.Entities;
 [Table("Clientes")]
 public class Cliente
